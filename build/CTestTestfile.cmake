1538bcb0bcd7e673dcd25b718cdd309ed303db45
Max <max.marika.brock@gmail.com>
2025-11-03 00:54:44 -0800

    removed somthing

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/max/UCSC/CMPM 123 - Advanced Programing/connect-4-starter-main
-# Build directory: /Users/max/UCSC/CMPM 123 - Advanced Programing/connect-4-starter-main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
